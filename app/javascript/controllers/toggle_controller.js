be7ff7d4d816f69ab92c64b4746fbd7425755d05
Braden Mason <braden142012@gmail.com>
2025-04-23 21:31:43 -0500

    Add new job button hidden until new job created or cancelled

--- app/javascript/controllers/toggle_controller.js
+++ app/javascript/controllers/toggle_controller.js
@@ -1,9 +1,11 @@
 import { Controller } from "@hotwired/stimulus";
 
 export default class extends Controller {
-  static targets = ["toggleable"];
+  static targets = ["toggleable", "button"];
 
   toggle() {
     this.toggleableTarget.classList.toggle("d-none");
+
+    this.buttonTarget.classList.toggle("d-none");
   }
 }
